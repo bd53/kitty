ddd4e9c5eb213600a0fb1e4879dee39c9e8ca8c6
bd53 <lj6976294@mail.com>
2025-11-18 19:49:13 -0500

    prefix unused params

--- tab_bar.py
+++ tab_bar.py
@@ -7,26 +7,23 @@ from kitty.tab_bar import DrawData, ExtraData, TabBarData, as_rgb, draw_attribut
 opts = get_options()
 timer_id = None
 
-def draw_tab(draw_data: DrawData, screen: Screen, tab: TabBarData, before: int, max_title_length: int, index: int, is_last: bool, extra_data: ExtraData) -> int:
+def draw_tab(draw: DrawData, screen: Screen, tab: TabBarData, _state: int, _length: int, index: int, last: bool, extra: ExtraData) -> int:
     global timer_id
     if timer_id is None:
         timer_id = add_timer(lambda _: get_boss().active_tab_manager.mark_tab_bar_dirty() if get_boss().active_tab_manager else None, 1, True)
     clock = datetime.now().strftime("%H:%M")
-    date = datetime.now().strftime("     %m.%d.%y")
-    right_cells = [(as_rgb(color_as_int(opts.color15)), clock), (as_rgb(color_as_int(opts.color8)), date)]
-    right_status_length = sum(len(cell[1]) for cell in right_cells)
+    date = datetime.now().strftime("%m.%d.%y")
+    status_right = [(as_rgb(color_as_int(opts.color15)), clock), (as_rgb(color_as_int(opts.color8)), "     " + date)]
+    status_length = sum(len(text) for _, text in status_right)
     tab_bg = screen.cursor.bg
     tab_fg = screen.cursor.fg
-    default_bg = as_rgb(int(draw_data.default_bg))
-    next_tab_bg = default_bg
-    needs_soft_separator = False
-    if extra_data.next_tab:
-        next_tab_bg = as_rgb(draw_data.tab_bg(extra_data.next_tab))
-        needs_soft_separator = next_tab_bg == tab_bg
-    if screen.cursor.x < screen.columns - right_status_length:
+    default_bg = as_rgb(int(draw.default_bg))
+    next_tab_bg = as_rgb(draw.tab_bg(extra.next_tab)) if extra.next_tab else default_bg
+    needs_soft_separator = next_tab_bg == tab_bg
+    if screen.cursor.x < screen.columns - status_length:
         screen.draw(" ")
         screen.cursor.bg = tab_bg
-        draw_title(draw_data, screen, tab, index)
+        draw_title(draw, screen, tab, index)
         if not needs_soft_separator:
             screen.draw(" ")
             screen.cursor.fg = tab_bg
@@ -36,16 +33,16 @@ def draw_tab(draw_data: DrawData, screen: Screen, tab: TabBarData, before: int, 
             if tab_bg == tab_fg:
                 screen.cursor.fg = default_bg
             elif tab_bg != default_bg:
-                c1 = draw_data.inactive_bg.contrast(draw_data.default_bg)
-                c2 = draw_data.inactive_bg.contrast(draw_data.inactive_fg)
+                c1 = draw.inactive_bg.contrast(draw.default_bg)
+                c2 = draw.inactive_bg.contrast(draw.inactive_fg)
                 if c1 < c2:
                     screen.cursor.fg = default_bg
             screen.cursor.fg = prev_fg
-    if is_last:
+    if last:
         draw_attributed_string(Formatter.reset, screen)
-        screen.cursor.x = screen.columns - right_status_length
+        screen.cursor.x = screen.columns - status_length
         screen.cursor.fg = 0
-        for color, text in right_cells:
+        for color, text in status_right:
             screen.cursor.fg = color
             screen.draw(text)
         screen.cursor.bg = 0
